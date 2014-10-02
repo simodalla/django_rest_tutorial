4ac66368ab610b4704b0390fda253a53f5d36c58
Simone Dalla <simodalla@gmail.com>
2014-10-02 14:52:21 +0200

    work in progress quickstart..

---
+++ django_rest_tutorial/snippets/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns(
+    'snippets.views',
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+)
